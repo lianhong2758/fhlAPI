6955c166e6a1b0ae1a519f9fde62d4d76478d8a1
lianhong <2758988938@qq.com>
2024-04-16 19:47:38 +0800

    改为post请求

--- server/server.go
+++ server/server.go
@@ -10,7 +10,7 @@ func RunGin(f *fhl.FHL) {
 	gin.SetMode(gin.DebugMode)
 	r := gin.Default() //初始化
 	r.GET("/", func(ctx *gin.Context) { ctx.JSON(200, gin.H{"msg": "hello"}) })
-	r.GET("/gettopic", GetTopic(f))
-	r.GET("/answer",  UpAnswer(f))
+	r.POST("/gettopic", GetTopic(f))
+	r.POST("/answer",  UpAnswer(f))
 	r.Run("0.0.0.0:8080")
 }
